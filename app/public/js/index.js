ddb457f956d5de6504f2feac8574943c2c49d523
jfolz <jackfolz98@gmail.com>
2021-09-21 14:16:36 -0400

    Creating index.js based off the professors code

---
+++ app/public/js/index.js
@@ -0,0 +1,39 @@
+// Source: https://github.com/tag/msis-in-class-2021/blob/blue-2021/public/js/index.js
+
+
+const personApp = {
+    data() {
+      return {
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.result.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            //Method 1:
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.result = json.results[0];
+                console.log("C");
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchUserData();
+    }
+
+  }
+  
+  Vue.createApp(personApp).mount('#personApp');
