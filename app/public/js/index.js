700ff2c7de3b51f705673dc06dbc92c2a7a02328
jfolz <jackfolz98@gmail.com>
2021-10-11 20:56:14 -0400

    changes to JS file

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,13 +1,13 @@
-// Source: https://github.com/tag/msis-in-class-2021/blob/blue-2021/public/js/index.js
+/* // Source: https://github.com/tag/msis-in-class-2021/blob/blue-2021/public/js/index.js
 
 
 const personApp = {
     data() {
       return {
-        result: {},
-        student: {},
-        offer: {},
-        //add selected students here
+        result: [],
+        student: [],
+        offer: [],
+        selectStudents: null
       }
     },
     computed: {
@@ -74,4 +74,66 @@ const personApp = {
 
   }
   
-  Vue.createApp(personApp).mount('#personApp');
+  Vue.createApp(personApp).mount('#personApp'); */
+
+
+const SomeApp = {
+    data() {
+    return {
+        students: [],
+        selectedStudent: null,
+        offers: []
+    }
+    },
+    computed: {},
+    methods: {
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        },
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        selectStudent(s) {
+            if (s == this.selectedStudent) {
+                return;
+            }
+            this.selectedStudent = s;
+            this.offers = [];
+            this.fetchOfferData(this.selectedStudent);
+        },
+        fetchStudentData() {
+            fetch('/api/student/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.students = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        fetchOfferData(s) {
+            console.log("Fetching offer data for ", s);
+            fetch('/api/offer/?student=' + s.id)
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.offers = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchStudentData();
+    }
+
+    }
+
+    Vue.createApp(SomeApp).mount('#offerApp');
